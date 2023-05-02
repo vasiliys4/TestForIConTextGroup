d49038748929c9c9fd7f131d730c5233854a814c
Сентемов Василий <sentemovv55@gmail.com>
2023-05-03 01:09:33 +0400

    Добавьте файлы проекта.

---
+++ TestForIConTextGroup/Hendler.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TestForIConTextGroup
+{
+    internal class Hendler
+    {
+        List<Employees> ListOfEmployees = new List<Employees>();
+        public void DeserializeJSON(string path)
+        {
+            ListOfEmployees = System.Text.Json.JsonSerializer.Deserialize<List<Employees>>(File.ReadAllText(path));
+            if (ListOfEmployees == null)
+            {
+                throw new Exception("Файл с сотрудниками пустой");
+            }
+        }
+        public void SerializeJSON(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.Formatting = Newtonsoft.Json.Formatting.Indented;
+                new Newtonsoft.Json.JsonSerializer().Serialize(writer, ListOfEmployees);
+            }
+        }
+        public void Add(Employees employee)
+        {
+            employee.Id = ListOfEmployees.Max(x => x.Id) + 1;
+            ListOfEmployees.Add(employee);
+        }
+        public void GetAll()
+        {
+            for (int i = 0; i < ListOfEmployees.Count; i++)
+            {
+                Console.WriteLine($"Id = {ListOfEmployees[i].Id}, FirstName = {ListOfEmployees[i].FirstName}, LastName = {ListOfEmployees[i].LastName}, SalaryPerHour = {ListOfEmployees[i].SalaryPerHour}");
+            }
+        }
+        public Employees Get(int id)
+        {
+            ListOfEmployees.FirstOrDefault(x => x.Id == id);
+            foreach (var employee in ListOfEmployees)
+            {
+                if (employee.Id == id)
+                {
+                    Console.WriteLine($"Id = {ListOfEmployees[id].Id}, FirstName = {ListOfEmployees[id].FirstName}, LastName = {ListOfEmployees[id].LastName}, SalaryPerHour = {ListOfEmployees[id].SalaryPerHour}");
+                    return ListOfEmployees[id];
+                }
+            }
+            Console.WriteLine("нет такого сотрудника");
+            return null;
+        }
+        public void Update(int id, Employees Employee)
+        {
+            var employee = ListOfEmployees.FirstOrDefault(x => x.Id == id);
+            if (employee != null)
+            {
+                if (Employee.Id != 0)
+                {
+                    employee.Id = Employee.Id;
+                }
+                if(Employee.FirstName != null)
+                {
+                    employee.FirstName = Employee.FirstName;
+                }
+                if(Employee.LastName != null)
+                {
+                    employee.LastName = Employee.LastName;
+                }
+                if (Employee.SalaryPerHour != 0)
+                {
+                    employee.SalaryPerHour = Employee.SalaryPerHour;
+                }               
+            }
+            else
+            {
+                Console.WriteLine("Такого id нет");
+            }
+        }
+        public void Delete(int id)
+        {
+            foreach (var employee in ListOfEmployees)
+            {
+                if (employee.Id == id)
+                {
+                    ListOfEmployees.RemoveAll(x => x.Id == id);
+                    return;
+                }
+            }
+            Console.WriteLine("Нет такого сотрудника");
+        }
+    }
+}
---
+++ TestForIConTextGroup/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TestForIConTextGroup;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? Path = Console.ReadLine();
+        if (Path[0] == '"') 
+        {
+            Path = Path.Trim(new char[] { '"' });
+        }
+        Hendler hendler = new Hendler();
+        hendler.DeserializeJSON(Path);
+        string[] operations = GetComands(args).ToArray();
+        for (int i = 0; i < operations.Length; i++)
+        {
+            switch (operations[i])
+            {
+                case "-add":
+                    hendler.Add(GetByValue(args));
+                    hendler.SerializeJSON(Path);
+                    break;
+                case "-update":
+                    hendler.Update(Convert.ToInt32(args[1].Split(':')[1]), GetByValue(args));
+                    hendler.SerializeJSON(Path);
+                    break;
+                case "-get":
+                    hendler.Get(GetingId(args));
+                    break;
+                case "-delete":
+                    hendler.Delete(GetingId(args));
+                    hendler.SerializeJSON(Path);
+                    break;
+                case "-getall":
+                    hendler.GetAll();
+                    break;
+            }
+        }
+    }
+    private static Queue<string> GetComands(string[] args)
+    {
+        var list = new Queue<string>();
+        string[] operations = { "-add", "-get", "-getall", "-update", "-delete" };
+        foreach (string arg in args)
+        {
+            foreach (string operation in operations)
+            {
+                if (arg == operation)
+                {
+                    list.Enqueue(operation);
+                }
+            }
+        }
+        return list;
+    }
+    private static Employees GetByValue(string[] args)
+    {
+        Employees employee = new Employees();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i].Split(":")[0] == "Id")
+            {
+                employee.Id = Convert.ToInt32(args[i].Split(":")[1]);
+            }
+            else if (args[i].Split(":")[0] == "FirstName")
+            {               
+                employee.FirstName = args[i].Split(":")[1];
+            }
+            else if (args[i].Split(":")[0] == "LastName")
+            {
+                employee.LastName = args[i].Split(":")[1];
+            }
+            else if (args[i].Split(":")[0] == "Salary")
+            {
+                string valueStr = args[i].Split(":")[1];
+                valueStr = valueStr.Replace(".", ",");
+                if (valueStr[valueStr.Length - 1] == ',')
+                {
+                    valueStr = valueStr.Remove(valueStr.Length - 1);
+                }
+                employee.SalaryPerHour = Convert.ToDecimal(valueStr);
+            }
+        }
+        return employee;
+    }
+    private static int GetingId(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i].Split(':')[0] == "Id")
+            {
+                string number = args[i].Split(':')[1];
+                int id = int.Parse(number);
+                return id;
+            }
+        }
+        return 0;
+    }
+}
